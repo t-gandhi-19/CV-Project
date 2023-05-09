28cbe00f392d45dc26c755065fedde78b9685a4c
Tarushi Gandhi <89021506+t-gandhi-19@users.noreply.github.com>
2023-05-09 06:25:41 +0530

    rectification_R vala

--- Organized code/stereo.py
+++ Organized code/stereo.py
@@ -53,3 +53,48 @@ def Two_Image_Stereo(Image1 , Image2 , Kernel_size=5 , Kernel_Method=None):
     print("Relative_R2 = " , Relative_R2)
     print("Relative_T2 = " , Relative_T2)
     print("Baseline2 = " , Baseline2)
+    
+
+def compute_rectification_R(EPS, T_ji):
+    '''T_ji - 3x1'''
+    
+    e_i = T_ji.squeeze(-1) / ( T_ji.squeeze(-1)[1] + EPS )
+    # squeezed_T_ji = []
+    # for i in range(len(T_ji)):
+    #     if T_ji.shape[i] != 1:
+    #         squeezed_T_ji.append(T_ji[i])
+    
+    # divisor = squeezed_T_ji[1] + EPS     
+    # e_i = []
+    # for i in range(len(squeezed_T_ji)):
+    #     e_i.append(squeezed_T_ji[i] / divisor)
+    
+    es = []   
+    norm = [np.sqrt(np.sum(np.square(T_ji + EPS)))]
+    e_2 = T_ji / norm[0]
+    es.append(e_2)
+    
+    e_1 = np.zeros((3,1))
+    e_1[0] = e_2[1]*1
+    e_1[1] = -e_2[0]*1
+    e_1[2] = 0
+    norm.append(np.sqrt(np.sum(np.square(e_1 + EPS))))
+    e_1 = e_1 / norm[1]
+    es.append(e_1)
+    
+    e_3 = np.zeros(3)
+    e_3[0] = e_1[1]*e_2[2] - e_1[2]*e_2[1]
+    e_3[1] = e_1[2]*e_2[0] - e_1[0]*e_2[2]
+    e_3[2] = e_1[0]*e_2[1] - e_1[1]*e_2[0]
+    norm.append(np.sqrt(np.sum(np.square(e_3 + EPS))))
+    e_3 = e_3 / norm[2]
+    es.append(e_3)
+    
+    R_irect = np.zeros((3, 3))
+    print(es[1].shape)
+    R_irect[0, :] = es[1].reshape((3))
+    R_irect[1, :] = es[0].reshape((3))
+    R_irect[2, :] = es[2].reshape((3))
+    
+    return R_irect
+    
